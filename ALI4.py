bac4a131291022af04809a6545caf632ebf07428
Omarail1 <116556590+Omarail1@users.noreply.github.com>
2022-11-05 14:21:18 +0300

    Update ALI4.py

--- ALI4.py
+++ ALI4.py
@@ -3,7 +3,7 @@ import requests
 import os,random,string
 from os import system
 import subprocess as sp
-ctypes.windll.kernel32.SetConsoleTitleA("Powered BY ALI SHAREEF")
+# ctypes.windll.kernel32.SetConsoleTitleA("Powered BY ALI SHAREEF")
 system("title " + "Powered By ALI SHAREEF ")
 # os.system('pip install faker')
 # os.system('pip install colorama')
